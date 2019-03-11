090ba220aea14f222861c86ceb4b691a0aad09bf
David <david.m.neuman@gmail.com>
2019-03-11 21:44:40 +0000

    Added secrets to .gitignore. Modified downloadAvatars to access the authentication token.

--- downloadAvatars.js
+++ downloadAvatars.js
@@ -1,10 +1,14 @@
 var request = require('request');
 var fs = require('fs');
-
+var token = require('./secrets');
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
   var url = 'https://api.github.com/repos/' + repoOwner + '/' + repoName + "/contributors";
+  headers = {
+    'User-Agent': 'request',
+    'Authorization': token.GITHUB_TOKEN
+  }
   request(url, function (err, res, body) {
     cb(err, body);
   })
