80be21c19e9a9e6159ae042eff338149f510f9b8
David <david.m.neuman@gmail.com>
2019-03-11 22:19:16 +0000

    Modified getRepoContributors function - request now will parse the body. The function call has been modified to iterate through the Avatar URLs and print them to the console.

--- downloadAvatars.js
+++ downloadAvatars.js
@@ -1,31 +1,27 @@
 var request = require('request');
 var fs = require('fs');
 var token = require('./secrets');
+console.log(token.GITHUB_TOKEN);
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  var url = 'https://api.github.com/repos/' + repoOwner + '/' + repoName + "/contributors";
-  headers = {
-    'User-Agent': 'request',
-    'Authorization': token.GITHUB_TOKEN
+  var options = {
+    url: 'https://api.github.com/repos/' + repoOwner + '/' + repoName + "/contributors",
+    headers: {
+      'User-Agent': 'request',
+      'Authorization': token.GITHUB_TOKEN
+    }
   }
-  request(url, function (err, res, body) {
-    cb(err, body);
-  })
-          // .on('error', function (error) {
-            // console.log('Error: ' + error);
-          // })
-          // .on ('response', function(response) {
-            // console.log('Contributor list populating...');
-            // console.log('Reponse status code: ' + response.statusCode);
-            // console.log('Reponse status message: ' + response.statusMessage);
-          // })
-          // .on('end', function () {
-            // console.log('Contributor list has been downloaded!');
-          // });
+    request(options, function (err, res, body) {
+      parsedBody = JSON.parse(body);
+      cb(err, parsedBody);
+    });
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
-  console.log("Result:", result);
+  for (var url in result) {
+    console.log("Avatar URL: " + result[url]["avatar_url"]);
+  }
+  // console.log("Result:", result);
 });
