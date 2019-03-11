3a3c31a414ebdebb3b19933949245008c4251079
David <david.m.neuman@gmail.com>
2019-03-11 21:27:05 +0000

    Added 'url' variable to more dyanamically store repo owner and name information when requesting contributors for a given repo.

--- downloadAvatars.js
+++ downloadAvatars.js
@@ -3,3 +3,25 @@ var fs = require('fs');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
+function getRepoContributors(repoOwner, repoName, cb) {
+  var url = 'https://api.github.com/repos/' + repoOwner + '/' + repoName + "/contributors";
+  request(url, function (err, res, body) {
+    cb(err, body);
+  })
+          // .on('error', function (error) {
+            // console.log('Error: ' + error);
+          // })
+          // .on ('response', function(response) {
+            // console.log('Contributor list populating...');
+            // console.log('Reponse status code: ' + response.statusCode);
+            // console.log('Reponse status message: ' + response.statusMessage);
+          // })
+          // .on('end', function () {
+            // console.log('Contributor list has been downloaded!');
+          // });
+}
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
